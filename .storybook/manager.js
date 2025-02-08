a8e595fd1f2d0a646ec02c1cea6bf9581964dacd
QuentinSoon <150387351+QuentinSoon@users.noreply.github.com>
2025-02-08 18:38:02 +0100

    add storybook w theme

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import MooyaLibTheme from './mistal';
+
+addons.setConfig({
+  theme: MooyaLibTheme,
+});
---
+++ .storybook/mistal.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  fontBase: '"Inter", sans-serif',
+  brandTitle: 'Portfolio Library',
+  brandUrl: '#',
+  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self',
+});
